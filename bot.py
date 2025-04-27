7f7c44fd2bfb75c88e71c6296e5f917e8d7195a4
botcreator48 <33tri3x@gmail.com>
2025-04-28 02:31:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,17 @@
-import asyncio
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-import os
-
-# Получаем токен из переменных окружения
-TOKEN = os.getenv("7770466925: AAF1vtWtw8x—6cABnhDG2
-P3i7nw0PGxvzds")
-
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я работаю!")
-
+    await update.message.reply_text("Привет! Я твой бот! Напиши мне что-нибудь.")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Ты сказал: {update.message.text}")
+    await update.message.reply_text(update.message.text)
 
-
-async def main():
+def main():
+    TOKEN = os.getenv("TELEGRAM_TOKEN")
     if not TOKEN:
-        print("Ошибка: токен не найден. Убедись, что TELEGRAM_TOKEN задан в настройках окружения.")
-        return
+        raise ValueError("Переменная окружения TELEGRAM_TOKEN не найдена!")
 
     app = ApplicationBuilder().token(TOKEN).build()
 
@@ -28,8 +19,7 @@ async def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     print("Бот запущен...")
-    await app.run_polling()
-
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
